cf55330ef208efbf89c1ed831198883b3f238d37
3d510 <3dmailboxes@gmail.com>
2017-04-12 00:30:58 +0200

    add file to Task 6

---
+++ Task6/src/MainSimulation.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class MainSimulation extends GlobalSimulation{
+ 
+    public static void main(String[] args) throws IOException {
+   
+    	double[] arrTimes = {1,2,5}; 
+    	
+    	for (int i=0; i<3; i++) {
+    		time = 0;
+        	Event actEvent;
+        	State actState = new State(arrTimes[i]); // The state that shoud be used
+
+        	// Some events must be put in the event list at the beginning
+        	insertEvent(ARRIVEQ1, 0);  
+            insertEvent(MEASURE, 5);
+        	// The main simulation loop
+        	while (actState.noMeasurements<1000){
+        		actEvent = eventList.fetchEvent();
+        		time = actEvent.eventTime;
+        		actState.treatEvent(actEvent);
+        	}
+        	System.out.printf("Interarrival time for Q1: %f\n", arrTimes[i]);
+        	System.out.printf("Mean number of customers in Q2: %f\n", 1.0*actState.curNoInQ2Sum/actState.noMeasurements);
+        	System.out.printf("Probability of rejected customers: %f\n", actState.noRejected*1.0/actState.noArrivals);
+        	System.out.println();
+    	}
+    	
+    }
+}
---
+++ Task6/src/State.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class State extends GlobalSimulation{
+	
+	// Here follows the state variables and other variables that might be needed
+	// e.g. for measurements
+	public int numberInQ1 = 0, numberInQ2 = 0, curNoInQ2Sum = 0, noMeasurements = 0;
+	public int noArrivals = 0, noRejected = 0;
+	private double interArrTime;
+	private double q1MeanServiceTime = 2.1, q2ServiceTime = 2, measureMeanTime = 5;
+
+	Random slump = new Random(); // This is just a random number generator
+	
+	public State(double interArrTime) {
+		this.interArrTime = interArrTime;
+	}
+	
+	// The following method is called by the main program each time a new event has been fetched
+	// from the event list in the main loop. 
+	public void treatEvent(Event x) {
+//		x.show();
+		switch (x.eventType){
+			case ARRIVEQ1:
+				arrivalq1();
+				break;
+			case DEPARTQ1:
+				departq1();
+				break;
+			case DEPARTQ2:
+				departq2();
+				break;
+			case MEASURE:
+				measure();
+				break;
+		}
+	}
+	
+	
+	// The following methods defines what should be done when an event takes place. This could
+	// have been placed in the case in treatEvent, but often it is simpler to write a method if 
+	// things are getting more complicated than this.
+	
+	private void arrivalq1(){
+		noArrivals++;
+		if (numberInQ1 == Q1CAPACITY) {
+			noRejected++;
+			return;
+		}
+		if (numberInQ1 == 0)
+			insertEvent(DEPARTQ1, time + exp(q1MeanServiceTime));
+		numberInQ1++;
+		insertEvent(ARRIVEQ1, time + interArrTime);
+	}
+	
+	private void departq1(){
+		numberInQ1--;
+		if (numberInQ2 == 0) 
+			insertEvent(DEPARTQ2, time + q2ServiceTime);
+		numberInQ2++;
+		if (numberInQ1 > 0)
+			insertEvent(DEPARTQ1, time + exp(q1MeanServiceTime));
+	}
+	
+	private void departq2() {
+		numberInQ2--;
+		if (numberInQ2 > 0)
+			insertEvent(DEPARTQ2, time + q2ServiceTime);
+	}
+	
+	private void measure(){
+		curNoInQ2Sum = curNoInQ2Sum + numberInQ2;
+		noMeasurements++;
+		insertEvent(MEASURE, time + exp(measureMeanTime));
+	}
+	
+	public double exp(double mean) {
+		return -Math.log(1-slump.nextDouble())*mean;
+	}
+}
